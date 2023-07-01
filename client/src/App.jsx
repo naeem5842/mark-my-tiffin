e37ec88a2083b9310a139cc3ca266c6c6fafa98c
Naeem Shikalgar <naeemshikalgar007@gmail.com>
2023-07-01 08:21:24 +0530

    staged all files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./components/Login/Login";
+// import SignUp from "./components/Signup/Signup";
+
+
+import './App.css'
+function App() {
+  return <Login />;
+}
+
+export default App;
